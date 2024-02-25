e4fdff37d9cca511de837b75e031562d15fc89ff
amresh012 <73485115+amresh012@users.noreply.github.com>
2024-02-25 11:38:04 +0530

    fix: deployemnet error fixed

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/CommanComponent/Footer/Footer'
-import Navbar from '../components/CommanComponent/NavBar/Navbar'
+import Navbar from '../components/CommanComponent/Navbar/Navbar'
 const Layout = () => {
     return (
         <>
